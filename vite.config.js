8404bd7f00789d7ce3f3ddac7e8dcad1e3ded974
Camirrix <brunocamirra@gmail.com>
2025-03-20 19:37:40 -0400

    Intento arreglo de vercel.json y vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,7 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css',
-                    'resources/js/app.js',
-                    'public/css/principal.css',
-                    'public/js/hide.js',
-                    'public/js/Scroll.js',
-                    'public/js/modal.js',
-                    'public/js/multipaso.js',
-                    'public/js/evaluacionInputs.js',],
+            input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
     ],
